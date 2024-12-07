11c229296ef0b0e90fbfe5220ae8625146ec3434
Sagar Sharma <tech.sagarsh@gmail.com>
2024-12-07 17:54:51 +0530

    fix: channel options list

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,43 +1,27 @@
-// import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import './header.css'
-// const Dropdown =()=>{
-//   const[selectedOption,setSelectedOption] =useState('');
-//   const handelSelectChange =(e)=>{
-//     setSelectedOption(e.target.value)
-//   };
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
-
-// const toggleMenu = () => {
-//   setIsOpen(!isOpen);
-// };
-
+import { useNavigate } from "react-router-dom";
+import "./header.css";
 
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <div className='header'>
-      <h1 className='name' onClick={() => navigate('/')}>HealthCare</h1>
-      <ul>
-        <li htmlFor='channel'>Channels </li>
-        <select name='channel' id='channel' onClick={() => navigate("/create-channel")}> 
-              <option value='myChannel'><span >My Channel</span></option>
-              <option value='Public Channel'>Public Channel</option>
-              <option value='Private Channel'>Private Channel</option>
-        </select>
-       
-        
+    <div className="header">
+      <h1 className="name" onClick={() => navigate("/")}>
+        HealthCare
+      </h1>
+      <ul className="header-links">
+        <li className="channel-options-wrapper" htmlFor="channel">Channels
+          <ul className="channel-options">
+            <li onClick={() => navigate("/create-channel")}>Create Channel</li>
+            <li>My Channels</li>
+          </ul>
+        </li>
         <li>App</li>
         <li>Community</li>
         <li>Support</li>
-        
       </ul>
-      <div className='login-button' onClick={() => navigate('/login')}>Login</div>
+      <div className="login-button" onClick={() => navigate("/login")}>
+        Login
+      </div>
     </div>
-  )
+  );
 }
