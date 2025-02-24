6c437fb2f74f6bccd2a8de4c70f406dbbccd86e7
Sagar Sharma <tech.sagarsh@gmail.com>
2025-02-24 23:38:56 +0530

    fix: remove hardcoded threshold values

--- src/Pages/Channel/Channel.jsx
+++ src/Pages/Channel/Channel.jsx
@@ -40,7 +40,7 @@ const Channel = () => {
         <div className="chart-container">
           <div className="chart-wrapper">
           <h1>{channelData.name}</h1>
-          <Chart fieldName={channelData.field1} data={channelData.fieldData} />
+          <Chart fieldName={channelData.field1} data={channelData.fieldData} minThresholdValue={channelData.minThreshold} maxThresholdValue={channelData.maxThreshold} />
           </div>
           <div>
             <ApiInfo apiKey={channelData.apiKey} fieldName={channelData.field1} />
