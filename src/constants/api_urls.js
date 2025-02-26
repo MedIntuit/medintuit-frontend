f713329eecff96a553c4e33b7575ee5af1573309
Mayank Rathore <mayankrathore2104@gmail.com>
2025-02-26 18:06:42 +0530

    getting humidity and temperature from BE

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://medintuit-backend-2-0.onrender.com";
-// export const BASE_URL = "http://localhost:8080"; // uncomment it if working with local backend 
+// export const BASE_URL = "https://medintuit-backend-2-0.onrender.com";
+export const BASE_URL = "http://localhost:8080"; // uncomment it if working with local backend 
 export const SIGNUP_URL = "api/auth/signup";
 export const LOGIN_URL = "api/auth/signin";
 export const CHANNEL_URL = "user-channels";
