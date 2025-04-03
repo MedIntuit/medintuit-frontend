4047fc045ba8390c45360d0c60e9eb0713b6a41d
Sagar Sharma <tech.sagarsh@gmail.com>
2025-04-03 17:10:22 +0530

    chore: update backend api url based on environment

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,5 +1,4 @@
-// export const BASE_URL = "https://medintuit-backend-2-0.onrender.com";
-export const BASE_URL = "http://localhost:8080"; // uncomment it if working with local backend 
+export const BASE_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:8080"; 
 export const SIGNUP_URL = "api/auth/signup";
 export const LOGIN_URL = "api/auth/signin";
 export const CHANNEL_URL = "user-channels";
