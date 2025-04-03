a00b3f3023880eee257ee44903a5c1fecba2255b
Sagar Sharma <tech.sagarsh@gmail.com>
2025-04-03 19:10:49 +0530

    fix: resolve logout error while refresh

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -3,16 +3,13 @@ import { createContext, useState, useEffect } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    return !!localStorage.getItem("token");
+  });
 
   useEffect(() => {
-    async function checkLoggedIn() {
-      const token = localStorage.getItem("token");
-      if (token) {
-        setIsAuthenticated(true);
-      }
-    }
-    checkLoggedIn();
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
   }, []);
 
   const login = (token) => {
